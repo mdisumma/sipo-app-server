795418b0caf5ce2ecfa4131ad7cbd7606f59f772
Marco Di Summa <mdisumma@yahoo.com>
2021-11-21 11:48:06 +0000

    min

--- public/admin/script.js
+++ public/admin/script.js
@@ -1,12 +1,31 @@
-const id = document.querySelector("#id");
-const product = document.querySelector("#product");
-const pack = document.querySelector("#pack");
-const image = document.querySelector("#image");
-const price = document.querySelector("#price");
-const productList = document.querySelector("#product_list");
-console.log(productList);
 //DOM
 window.addEventListener("DOMContentLoaded", () => {
+	const id = document.querySelector("#id");
+	const product = document.querySelector("#product");
+	const pack = document.querySelector("#pack");
+	const image = document.querySelector("#image");
+	const price = document.querySelector("#price");
+	const productList = document.querySelector("#product_list");
+	const submitBt = document.querySelector("#submit");
+
+	//SUBMIT
+	submitBt.addEventListener("click", (event) => {
+		fetch("http://localhost:3001", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				id: id.value,
+				name: product.value,
+				pack: pack.value,
+				image: image.value,
+				price: price.value,
+			}),
+		})
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log("error", error));
+	});
+
 	//DATA
 	fetch("http://localhost:3001/api")
 		.then((response) => response.json())
@@ -31,70 +50,46 @@ window.addEventListener("DOMContentLoaded", () => {
 							</div>
 				`;
 			});
+
+			//DELETE
+			const deleteBt = document.querySelector("#delete");
+			deleteBt.addEventListener("click", (event) => {
+				fetch("http://localhost:3001", {
+					method: "DELETE",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						id: id.value,
+						name: product.value,
+						pack: pack.value,
+						image: image.value,
+						price: price.value,
+					}),
+				})
+					.then((response) => response.text())
+					.then((result) => console.log(result))
+					.catch((error) => console.log("error", error));
+			});
+
+			//PUT
+			const updateBt = document.querySelector("#update");
+			console.log(updateBt);
+			updateBt.addEventListener("click", (e) => {
+				console.log(e);
+				// 	fetch("http://localhost:3001", {
+				// 		method: "PUT",
+				// 		headers: { "Content-Type": "application/json" },
+				// 		body: JSON.stringify({
+				// 			id: id.value,
+				// 			name: product.value,
+				// 			pack: pack.value,
+				// 			image: image.value,
+				// 			price: price.value,
+				// 		}),
+				// 	})
+				// 		.then((response) => response.text())
+				// 		.then((result) => console.log(result))
+				// 		.catch((error) => console.log("error", error));
+			});
 		})
 		.catch((error) => console.log("error", error));
-
-	//SUBMIT
-	const submitBt = document.querySelector("#submit");
-	submitBt.addEventListener("click", (event) => {
-		event.preventDefault();
-
-		fetch("http://localhost:3001", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				id: id.value,
-				name: product.value,
-				pack: pack.value,
-				image: image.value,
-				price: price.value,
-			}),
-		})
-			.then((response) => response.text())
-			.then((result) => console.log(result))
-			.catch((error) => console.log("error", error));
-	});
-
-	//DELETE
-	const deleteBt = document.querySelector("#delete");
-	deleteBt.addEventListener("click", (event) => {
-		event.preventDefault();
-
-		fetch("http://localhost:3001", {
-			method: "DELETE",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				id: id.value,
-				name: product.value,
-				pack: pack.value,
-				image: image.value,
-				price: price.value,
-			}),
-		})
-			.then((response) => response.text())
-			.then((result) => console.log(result))
-			.catch((error) => console.log("error", error));
-	});
-
-	//PUT
-
-	const updateBt = document.querySelector("#update");
-	updateBt.addEventListener("click", (event) => {
-		event.preventDefault();
-
-		fetch("http://localhost:3001", {
-			method: "PUT",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				id: id.value,
-				name: product.value,
-				pack: pack.value,
-				image: image.value,
-				price: price.value,
-			}),
-		})
-			.then((response) => response.text())
-			.then((result) => console.log(result))
-			.catch((error) => console.log("error", error));
-	});
 });
