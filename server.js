bac8a8221ed6143e352c9e6e8c664d602712e347
Marco Di Summa <mdisumma@yahoo.com>
2021-11-23 12:36:02 +0000

    min

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
-import Routes from "./src/routes/routes.js";
+import { createClient } from "@supabase/supabase-js";
+import { SERVICE_KEY, SUPABASE_URL } from "./src/key/key.js";
+import Api from "./src/routes/api.js";
+import Auth from "./src/routes/auth.js";
+import Product from "./src/routes/product.js";
 
 const server = express();
 const port = 3001;
@@ -10,7 +14,17 @@ server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 server.use(express.static("public"));
 
-Routes();
+const supabase = createClient(SUPABASE_URL, SERVICE_KEY);
+
+const products_table = "sipo_products";
+const users_table = "sipo_users";
+const orderBy = "name";
+
+Api(server, supabase, products_table, orderBy);
+
+Auth(server, supabase, users_table);
+
+Product(server, supabase, products_table);
 
 server.listen(port, () => {
 	console.log(`The server is listening on port ${port}.`);
