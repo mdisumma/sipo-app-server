9eede3744176e28c553d8d8ffff81600e71a677b
Marco Di Summa <mdisumma@yahoo.com>
2021-11-24 12:53:22 +0000

    min

--- public/user/user.js
+++ public/user/user.js
@@ -24,7 +24,7 @@ logOut.addEventListener("click", () => {
 			message: "Log out",
 		}),
 	};
-	fetch("http://localhost:3001/logout/", post)
+	fetch("http://localhost:3000/logout/", post)
 		.then((response) => response.text())
 		.then((result) => console.log(result))
 		.catch((error) => console.log("error", error));
@@ -79,7 +79,7 @@ const displayOrderList = (e) => {
 
 //DATA
 console.log("fetch starts here");
-fetch("http://localhost:3001/api")
+fetch("http://localhost:3000/api")
 	.then((response) => response.json())
 	.then((data) => {
 		console.log(data);
