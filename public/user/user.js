ad5e49df69b400575c5dd70c7d9d275e0a13d23e
Marco Di Summa <mdisumma@yahoo.com>
2021-11-24 18:21:17 +0000

    min

--- public/user/user.js
+++ public/user/user.js
@@ -10,45 +10,45 @@ const user = supabase.auth.user();
 console.log(user);
 
 //DOM
-// document.addEventListener("DOMContentLoaded", () => {
-const productList = document.querySelector("#product_list");
-const productOrder = document.querySelector("#product_order");
-const logOut = document.querySelector("#log_out");
+document.addEventListener("DOMContentLoaded", () => {
+	const productList = document.querySelector("#product_list");
+	const productOrder = document.querySelector("#product_order");
+	const logOut = document.querySelector("#log_out");
 
-//LOGOUT
-logOut.addEventListener("click", () => {
-	var post = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			message: "Log out",
-		}),
-	};
-	fetch("http://46.101.76.197/logout/", post)
-		.then((response) => response.text())
-		.then((result) => console.log(result))
-		.catch((error) => console.log("error", error));
-	window.location.href = `http://46.101.76.197/`;
-});
+	//LOGOUT
+	logOut.addEventListener("click", () => {
+		var post = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				message: "Log out",
+			}),
+		};
+		fetch("http://46.101.76.197/logout/", post)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log("error", error));
+		window.location.href = `http://46.101.76.197/`;
+	});
 
-//ORDER LIST
-const orderList = [
-	{
-		name: "Name",
-		number: "Item",
-		unit: "Unit Gr",
-		totUnit: "Tot Gr",
-		price: "Price £",
-		totPrice: "Tot £",
-		trash: "",
-	},
-];
+	//ORDER LIST
+	const orderList = [
+		{
+			name: "Name",
+			number: "Item",
+			unit: "Unit Gr",
+			totUnit: "Tot Gr",
+			price: "Price £",
+			totPrice: "Tot £",
+			trash: "",
+		},
+	];
 
-const displayOrderList = (e) => {
-	productOrder.innerHTML = "";
-	let i = 0;
-	orderList.map((item) => {
-		productOrder.innerHTML += `
+	const displayOrderList = (e) => {
+		productOrder.innerHTML = "";
+		let i = 0;
+		orderList.map((item) => {
+			productOrder.innerHTML += `
 		<ul id="${i++}">
 		<li class="product_name">${item.name}</li>
 		<li>${item.number}</li>
@@ -58,33 +58,33 @@ const displayOrderList = (e) => {
 		<li>${item.totPrice}</li>
 		<li class="trash"><button >${item.trash}</button></li>
 		</ul>`;
-	});
+		});
 
-	const trash = document.querySelector("#product_order");
+		const trash = document.querySelector("#product_order");
 
-	trash.addEventListener("click", (e) => {
-		const position = e.path[3].id;
-		console.log(position);
+		trash.addEventListener("click", (e) => {
+			const position = e.path[3].id;
+			console.log(position);
 
-		const deleteProduct = () => {
-			orderList.splice(position, 1);
-		};
-		console.log(orderList);
-		deleteProduct();
-		console.log(orderList);
+			const deleteProduct = () => {
+				orderList.splice(position, 1);
+			};
+			console.log(orderList);
+			deleteProduct();
+			console.log(orderList);
 
-		displayOrderList();
-	});
-};
+			displayOrderList();
+		});
+	};
 
-//DATA
-console.log("fetch starts here");
-fetch("http://46.101.76.197/api")
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data);
-		data.map((item) => {
-			productList.innerHTML += `
+	//DATA
+	console.log("fetch starts here");
+	fetch("http://46.101.76.197/api")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			data.map((item) => {
+				productList.innerHTML += `
 				<div key=${item.image} class="product">
 					<div class="product_image">
 						<img src=${item.image} alt=${item.name} />
@@ -114,36 +114,36 @@ fetch("http://46.101.76.197/api")
 					</div>
 				</div>
 				`;
-		});
+			});
 
-		//INPUTS
-		const addButton = document.querySelector("#product_list");
-		// console.log(addButton);
-		addButton.addEventListener("click", (e) => {
-			if (e.target.className === "add_Button") {
-				// console.log(e);
-				const productNumber = e.path[1].childNodes[1].value;
-				// console.log(productNumber);
+			//INPUTS
+			const addButton = document.querySelector("#product_list");
+			// console.log(addButton);
+			addButton.addEventListener("click", (e) => {
+				if (e.target.className === "add_Button") {
+					// console.log(e);
+					const productNumber = e.path[1].childNodes[1].value;
+					// console.log(productNumber);
 
-				const productName = e.path[2].children[1].children[0].innerText;
-				const productPack = e.path[2].children[1].children[2].innerText;
-				const productPrice = e.path[2].children[1].children[4].innerText;
+					const productName = e.path[2].children[1].children[0].innerText;
+					const productPack = e.path[2].children[1].children[2].innerText;
+					const productPrice = e.path[2].children[1].children[4].innerText;
 
-				const order = {
-					name: productName,
-					number: productNumber,
-					unit: productPack,
-					totUnit: productPack * productNumber,
-					price: productPrice,
-					totPrice: productPrice * productNumber,
-					trash: "<i class='far fa-trash-alt'></i>",
-				};
-				// console.log(order);
-				orderList.push(order);
-				// console.log(orderList);
-				displayOrderList();
-			}
-		});
-	})
-	.catch((error) => console.log("error", error));
-// });
+					const order = {
+						name: productName,
+						number: productNumber,
+						unit: productPack,
+						totUnit: productPack * productNumber,
+						price: productPrice,
+						totPrice: productPrice * productNumber,
+						trash: "<i class='far fa-trash-alt'></i>",
+					};
+					// console.log(order);
+					orderList.push(order);
+					// console.log(orderList);
+					displayOrderList();
+				}
+			});
+		})
+		.catch((error) => console.log("error", error));
+});
